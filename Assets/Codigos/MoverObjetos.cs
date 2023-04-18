f7ae1e7c9a7c174171f0eebbb7a0aa3709ba603a
Nehuen P <nehuenpeyrano@gmail.com>
2023-04-18 18:32:34 -0300

    17-4
    Los objetos se pueden mover del escenario, si son soltados en cualquier lugar que no sea en el caldero vuelven a su posición original. Cuando dos objetos entran en el caldero vuelven a su posición original y genera una poción.

--- Assets/Codigos/MoverObjetos.cs
+++ Assets/Codigos/MoverObjetos.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class MoverObjetos : MonoBehaviour
 {
+    public GameObject pocion;
     private bool enCaldero;
     private Vector2 posicionInicial;
     private GameObject objetoEnCaldero;
     private SpriteRenderer spriteRenderer;
     private BoxCollider2D boxCollider;
     private static List<GameObject> objetosEnCaldero = new List<GameObject>();
+    //private int contadorObjetosEnCaldero = 0;
 
     void Start()
     {
         posicionInicial = transform.position;
         spriteRenderer = GetComponent<SpriteRenderer>();
         boxCollider = GetComponent<BoxCollider2D>();
+        pocion.GetComponent<SpriteRenderer>().enabled = false; // Desactivar el SpriteRenderer de la poción al inicio
     }
 
     void OnMouseDown()
@@ -43,41 +46,56 @@ public class MoverObjetos : MonoBehaviour
 
     void OnMouseUp()
     {
-        GetComponent<SpriteRenderer>().sortingOrder = 1;
+      GetComponent<SpriteRenderer>().sortingOrder = 1;
 
-        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.zero);
+      RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Vector2.zero);
 
-        bool hitCaldero = false;
-        foreach (RaycastHit2D hit in hits)
-        {
-            if (hit.collider.CompareTag("Caldero"))
-            {
-                hitCaldero = true;
-                enCaldero = true;
-                objetoEnCaldero = hit.collider.gameObject;
-                objetosEnCaldero.Add(gameObject); // añadir objeto a la lista de objetos en el caldero
-                spriteRenderer.enabled = false;
-                boxCollider.enabled = false;
+      bool hitCaldero = false;
+      foreach (RaycastHit2D hit in hits)
+      {
+          if (hit.collider.CompareTag("Caldero"))
+          {
+              hitCaldero = true;
+              enCaldero = true;
+              objetoEnCaldero = hit.collider.gameObject;
+              objetosEnCaldero.Add(gameObject); // añadir objeto a la lista de objetos en el caldero
+              spriteRenderer.enabled = false;
+              boxCollider.enabled = false;
 
-                // Si hay más de un objeto en el caldero, devolver todos los objetos a su posición inicial
-                if (objetosEnCaldero.Count > 1)
-                {
-                    foreach (GameObject obj in objetosEnCaldero)
-                    {
-                        obj.GetComponent<SpriteRenderer>().enabled = true;
-                        obj.GetComponent<BoxCollider2D>().enabled = true;
-                        obj.transform.position = obj.GetComponent<MoverObjetos>().posicionInicial;
-                        obj.GetComponent<MoverObjetos>().enCaldero = false;
-                    }
+              if (objetosEnCaldero.Count == 2) // Verificar si se han agregado dos objetos al caldero
+              {
+                  // Devolver todos los objetos a su posición inicial
+                  foreach (GameObject obj in objetosEnCaldero)
+                  {
+                      obj.GetComponent<SpriteRenderer>().enabled = true;
+                      obj.GetComponent<BoxCollider2D>().enabled = true;
+                      obj.transform.position = obj.GetComponent<MoverObjetos>().posicionInicial;
+                      obj.GetComponent<MoverObjetos>().enCaldero = false;
+                  }
 
-                    objetosEnCaldero.Clear(); // Limpiar la lista de objetos en el caldero
-                }
-            }
-        }
+                  objetosEnCaldero.Clear(); // Limpiar la lista de objetos en el caldero
 
-        if (!hitCaldero)
-        {
-            transform.position = posicionInicial;
-        }
+                  // Generar la poción
+                  transform.position = posicionInicial;
+                  pocion.GetComponent<SpriteRenderer>().enabled = true;
+              }
+          }
+      }
+
+      if (!hitCaldero)
+      {
+          transform.position = posicionInicial;
+
+          // Desactivar el SpriteRenderer de la poción si no se agregaron ambos ingredientes
+          if (objetosEnCaldero.Count < 2)
+          {
+              pocion.GetComponent<SpriteRenderer>().enabled = false;
+          }
+      }
+  }
+    public void Devolver()
+    {
+        transform.position = posicionInicial;
     }
 }
+
