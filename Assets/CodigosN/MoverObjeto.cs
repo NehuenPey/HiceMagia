bc89ac0e4afa38be4f653b3078641f3523b89959
Nehuen P <nehuenpeyrano@gmail.com>
2023-05-05 22:33:35 -0300

    Cod

--- Assets/CodigosN/MoverObjeto.cs
+++ Assets/CodigosN/MoverObjeto.cs
@@ -6,7 +6,12 @@ public class MoverObjeto : MonoBehaviour
 {
     private bool enCaldero;
     private Tiempo timer;
-    public GameObject pocion;
+    public GameObject pocionRosa;
+    public GameObject pocionVerde;
+    public GameObject pocionVioleta;
+    public GameObject pocionNaranja;
+    public GameObject explosion;
+    public GameObject mariposa;
     private Vector2 posicionInicial;
     private GameObject objetoEnCaldero;
     private SpriteRenderer spriteRenderer;
@@ -64,60 +69,140 @@ public class MoverObjeto : MonoBehaviour
         {
             if (hit.collider.CompareTag("Caldero"))
             {
-                hitCaldero = true;
-                enCaldero = true;
-                objetoEnCaldero = hit.collider.gameObject;
-                objetosEnCaldero.Add(gameObject); // añadir objeto a la lista de objetos en el caldero
-                tirarencaldero.Post(gameObject);
-                Debug.Log("Total de objetos en caldero: " + objetosEnCaldero.Count);
+            hitCaldero = true;
+            enCaldero = true;
+            objetoEnCaldero = hit.collider.gameObject;
+            objetosEnCaldero.Add(gameObject); // añadir objeto a la lista de objetos en el caldero
+            Debug.Log("Total de objetos en caldero: " + objetosEnCaldero.Count);
+            tirarencaldero.Post(gameObject);
 
-                spriteRenderer.enabled = false;
-                boxCollider.enabled = false;
+            spriteRenderer.enabled = false;
+            boxCollider.enabled = false;
 
-                if (objetosEnCaldero.Count == 2) // Verificar si se han agregado dos objetos al caldero
+                // Verificar si se han agregado dos objetos al caldero
+                if (objetosEnCaldero.Count == 2)
                 {
-                    // Devolver todos los objetos a su posición inicial
-                    foreach (GameObject obj in objetosEnCaldero)
+                    // Verificar qué objetos se han combinado para generar la poción correspondiente
+                    if (objetosEnCaldero[0].CompareTag("Rojo") && objetosEnCaldero[1].CompareTag("Blanco") ||
+                        objetosEnCaldero[0].CompareTag("Blanco") && objetosEnCaldero[1].CompareTag("Rojo"))
                     {
-                        obj.GetComponent<SpriteRenderer>().enabled = true;
-                        obj.GetComponent<BoxCollider2D>().enabled = true;
-                        obj.transform.position = obj.GetComponent<MoverObjeto>().PosicionInicial;
-                        obj.GetComponent<MoverObjeto>().enCaldero = false;
-                    }
+                        // Generar la poción Rosa
+                        transform.position = posicionInicial;
+                        magiapocion.Post(gameObject);
+                        pocionRosa.GetComponent<SpriteRenderer>().enabled = true;
+                        objetosEnCaldero.Clear();
 
-                    // Generar la poción
+                        Puntuacion pocionManager = FindObjectOfType<Puntuacion>();
+                        pocionManager.IncrementarPuntuacion(5);
+                        Invoke("DesactivarPocionGenerada",tiempoPocionEnPantalla);
+                    }
+                else if (objetosEnCaldero[0].CompareTag("Rojo") && objetosEnCaldero[1].CompareTag("Azul") ||
+                    objetosEnCaldero[0].CompareTag("Azul") && objetosEnCaldero[1].CompareTag("Rojo")) // Verificar si se han combinado otros objetos para generar otras pociones
+                {
+                    // Generar la poción violeta
                     transform.position = posicionInicial;
                     magiapocion.Post(gameObject);
-                    pocion.GetComponent<SpriteRenderer>().enabled = true;
-                    Debug.Log("Pocion Generada");
-                    objetosEnCaldero.Clear(); // Limpiar la lista de objetos en el caldero
+                    pocionVioleta.GetComponent<SpriteRenderer>().enabled = true;
+                    objetosEnCaldero.Clear();
 
                     Puntuacion pocionManager = FindObjectOfType<Puntuacion>();
-                    pocionManager.IncrementarPuntuacion(2);
-                    Invoke("DesactivarPocionGenerada", tiempoPocionEnPantalla);
+                    pocionManager.IncrementarPuntuacion(5);
+                    Invoke("DesactivarPocionGenerada",tiempoPocionEnPantalla);
+                
+                }
+                else if (objetosEnCaldero[0].CompareTag("Rojo") && objetosEnCaldero[1].CompareTag("Amarillo") ||
+                    objetosEnCaldero[0].CompareTag("Amarillo") && objetosEnCaldero[1].CompareTag("Rojo")) // Verificar si se han combinado otros objetos para generar otras pociones
+                {
+                    // Generar la poción naranja
+                    transform.position = posicionInicial;
+                    magiapocion.Post(gameObject);
+                    pocionNaranja.GetComponent<SpriteRenderer>().enabled = true;
+                    objetosEnCaldero.Clear();
+
+                    Puntuacion pocionManager = FindObjectOfType<Puntuacion>();
+                    pocionManager.IncrementarPuntuacion(5);
+                    Invoke("DesactivarPocionGenerada",tiempoPocionEnPantalla);
+                
+                }
+                else if (objetosEnCaldero[0].CompareTag("Amarillo") && objetosEnCaldero[1].CompareTag("Azul") ||
+                    objetosEnCaldero[0].CompareTag("Azul") && objetosEnCaldero[1].CompareTag("Amarillo")) // Verificar si se han combinado otros objetos para generar otras pociones
+                {
+                    // Generar la poción verde
+                    transform.position = posicionInicial;
+                    magiapocion.Post(gameObject);
+                    pocionVerde.GetComponent<SpriteRenderer>().enabled = true;
+                    objetosEnCaldero.Clear();
+
+                    Puntuacion pocionManager = FindObjectOfType<Puntuacion>();
+                    pocionManager.IncrementarPuntuacion(5);
+                    Invoke("DesactivarPocionGenerada",tiempoPocionEnPantalla);
+                
+                }
+                else if (objetosEnCaldero[0].CompareTag("Amarillo") && objetosEnCaldero[1].CompareTag("Rojo") ||
+                    objetosEnCaldero[0].CompareTag("Rojo") && objetosEnCaldero[1].CompareTag("Amarillo")) // Verificar si se han combinado otros objetos para generar otras pociones
+                {
+                    // Generar la poción explosion
+                    transform.position = posicionInicial;
+                    magiapocion.Post(gameObject);
+                    explosion.GetComponent<SpriteRenderer>().enabled = true;
+                    objetosEnCaldero.Clear();
+
+                    Puntuacion pocionManager = FindObjectOfType<Puntuacion>();
+                    pocionManager.ReducirPuntuacion(3);
+                    Invoke("DesactivarPocionGenerada",tiempoPocionEnPantalla);
+                
+                }
+                else if (objetosEnCaldero[0].CompareTag("Blanco") && objetosEnCaldero[1].CompareTag("Azul") ||
+                    objetosEnCaldero[0].CompareTag("Azul") && objetosEnCaldero[1].CompareTag("Blanco")) // Verificar si se han combinado otros objetos para generar otras pociones
+                {
+                    // Generar la poción mariposas
+                    transform.position = posicionInicial;
+                    magiapocion.Post(gameObject);
+                    mariposa.GetComponent<SpriteRenderer>().enabled = true;
+                    objetosEnCaldero.Clear();
+
+                    Puntuacion pocionManager = FindObjectOfType<Puntuacion>();
+                    pocionManager.ReducirPuntuacion(2);
+                    Invoke("DesactivarPocionGenerada",tiempoPocionEnPantalla);
+                
+                }
+                else
+                    {
+                        // Si no se han combinado objetos que generen una poción, devolver los objetos a su posición inicial
+                        foreach (GameObject obj in objetosEnCaldero)
+                        {
+                            obj.GetComponent<SpriteRenderer>().enabled = true;
+                            obj.GetComponent<BoxCollider2D>().enabled = true;
+                            obj.transform.position = obj.GetComponent<MoverObjeto>().PosicionInicial;
+                            obj.GetComponent<MoverObjeto>().enCaldero = false;
+                        }
+                        transform.position = posicionInicial;
+                        objetosEnCaldero.Clear(); // Limpiar la lista de objetos en el caldero
+                        objetoEnCaldero.GetComponent<MoverObjeto>().spriteRenderer.enabled = true;
+                        objetoEnCaldero.GetComponent<MoverObjeto>().boxCollider.enabled = true;
+                    }
                 }
             }
+            if (!hitCaldero && objetosEnCaldero.Count < 2)
+            {
+                transform.position = posicionInicial;
+            }
         }
-
-        if (!hitCaldero && objetosEnCaldero.Count < 2)
-        {
-            transform.position = posicionInicial;
-        }
-    }
-
-    public void Devolver()
-    {
-        transform.position = posicionInicial;
-        enCaldero = false;
-        spriteRenderer.enabled = true;
-        boxCollider.enabled = true;
     }
 
     public Vector2 PosicionInicial {
         get { return posicionInicial; }
     }
+
     private void DesactivarPocionGenerada()
     {
-        pocion.GetComponent<SpriteRenderer>().enabled = false;
+        pocionRosa.GetComponent<SpriteRenderer>().enabled = false;
+        pocionVioleta.GetComponent<SpriteRenderer>().enabled = false;
+        pocionVerde.GetComponent<SpriteRenderer>().enabled = false;
+        pocionNaranja.GetComponent<SpriteRenderer>().enabled = false;
+        explosion.GetComponent<SpriteRenderer>().enabled = false;
+        mariposa.GetComponent<SpriteRenderer>().enabled = false;
+
     }
+
 }
